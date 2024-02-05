10b9f28e47afa6dee3ac56c4ac83fff2f7e5fa79
garbuzshay <shay.garbuz@e.braude.ac.il>
2024-02-05 15:40:57 +0200

    first commit

--- set_git.bat
+++ set_git.bat
@@ -7,5 +7,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/garbuzshay/MyNewApp.git
+git remote add origin https://github.com/garbuzshay/ex4.git
 git push -u origin main
